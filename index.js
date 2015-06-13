5ae339e51f4cdce71a7b9b05e249fa78a715acdd
Cari Westbrook <cari.westbrook@gmail.com>
2015-06-12 23:06:38 -0700

    Serves index html page

--- index.js
+++ index.js
@@ -2,19 +2,34 @@
 // Simple app for "Hello World"
 // ====================================================================
 
-var express = require('express');
-var app = express();
+// var express = require('express');
+// var app = express();
+
+// app.get('/', function(req, res){
+//   res.send("<h1>Hello, World.</h1>");
+// });
+
+// var server = app.listen(1337, function () {
+
+//   var host = server.address().address;
+//   var port = server.address().port;
+
+//   console.log('Example app listening at http://%s:%s', host, port);
+
+// });
+
+// ====================================================================
+// Serving a simple HTML file; CSS isn't being included?
+// ====================================================================
+
+var app = require('express')();
+var http = require('http').Server(app);
 
 app.get('/', function(req, res){
-  res.send("<h1>Hello, World.</h1>");
+  res.sendFile(__dirname + '/index.html');
 });
 
-var server = app.listen(1337, function () {
-
-  var host = server.address().address;
-  var port = server.address().port;
-
-  console.log('Example app listening at http://%s:%s', host, port);
-
+http.listen(1337, function() {
+ console.log("listening on *:1337");
 });
 
