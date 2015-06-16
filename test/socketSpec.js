b7c55259debbd2edfa50fc6a3a8b1a5962286c3f
Cari Westbrook <cari.westbrook@gmail.com>
2015-06-16 12:23:36 -0700

    Add test to listen for a new chat

--- test/socketSpec.js
+++ test/socketSpec.js
@@ -10,15 +10,16 @@ var options = {
 var chatUser1 = { username: "Cari" }
 var chatUser2 = { username: "Doug" }
 
-describe("Chat Server", function() {
-  it("Should broadcast a new user to all users", function() {
 
-    var client1 = io.connect(socketUrl, options);
+
+describe("Chat Server", function() {
+  var client1 = io.connect(socketUrl, options);
+  var client2 = io.connect(socketUrl, options);
+
+  it("Should broadcast a new user to all users", function() {
 
     client1.on('connection', function(data) {
       client1.emit('join', chatUser1.username);
-
-      var client2 = io.connect(socketUrl, options);
 
       client2.on('connection', function(data) {
         client2.emit('join', chatUser2.username);
@@ -26,7 +27,7 @@ describe("Chat Server", function() {
 
       client2.on('addUser', function(newUser) {
         newUser.should.equal(chatUser2.username);
-        chleint2.disconnect();
+        client2.disconnect();
       });
     });
 
@@ -40,6 +41,34 @@ describe("Chat Server", function() {
         done();
       }
     });
+  });
 
+  it("should listen for a new chat", function() {
+    client1.on('connection', function(data) {
+      client1.emit('join', chatUser1.username);
+
+      client2.on('connection', function(data) {
+        client2.emit('join', chatUser2.username);
+        client2.emit('sendChat', "Hello!");
+        client2.disconnect();
+      });
+
+      client2.on('sendChat', function(msg, newUser) {
+        msg.should.equal("Hello!");
+        username.should.equal(chatUser2.newUser);
+      });
+
+      var numChats = 0;
+      client1.on('sendChat', function(msg, newUser) {
+        numChats++;
+
+        if (numChats === 1) {
+          msg.should.equal("Hello!");
+          newUser.should.equal(chatUser2.username);
+          client1.disconnect();
+          done();
+        }
+      });
+    });
   });
 });
